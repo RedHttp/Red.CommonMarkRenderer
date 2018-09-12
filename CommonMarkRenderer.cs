1ba59630e41f53f6b5323fe7409a223ab8c53df5
rosenbjerg <rosenbjerg@users.noreply.github.com>
2018-09-12 22:58:21 +0200

    changes to NET Standard

--- CommonMarkRenderer.cs
+++ CommonMarkRenderer.cs
@@ -15,19 +15,16 @@ namespace Red.CommonMarkRenderer
         /// <param name="instance">The instance of the response</param>
         /// <param name="filePath">The path of the CommonMark file to be rendered</param>
         /// <param name="status">The status code for the response</param>
-        public static Task RenderFile(this Response instance, string filePath, HttpStatusCode status = HttpStatusCode.OK)
+        public static Task RenderFile(this Response instance, string filePath, HttpStatusCode status = HttpStatusCode.OK) => Task.Run(() =>
         {
-            instance.UnderlyingResponse.StatusCode = (int)status;
-            instance.UnderlyingResponse.ContentType = "text/html";
             using (var reader = new System.IO.StreamReader(filePath))
             using (var writer = new System.IO.StreamWriter(instance.UnderlyingResponse.Body))
             {
                 CommonMark.CommonMarkConverter.Convert(reader, writer);
             }
             instance.Closed = true;
-            return Task.CompletedTask;
-        }
-        
+        });
+
         /// <summary>
         ///     Renders a string containing CommonMark text to HTML and sends it
         /// </summary>
@@ -38,10 +35,8 @@ namespace Red.CommonMarkRenderer
         /// <returns></returns>
         public static async Task RenderString(this Response instance, string commonMarkText, string fileName = null, HttpStatusCode status = HttpStatusCode.OK)
         {
-            instance.UnderlyingResponse.StatusCode = (int)status;
-            instance.UnderlyingResponse.ContentType = "text/html";
             var html = CommonMark.CommonMarkConverter.Convert(commonMarkText);
-            await instance.SendString(html, "text/html", fileName, status);
+            await instance.SendString(html, "text/html", fileName, status: status);
         }
 
     }
